d31cabaffbcb094e7b2e054aaa06a14c2218f2fb
DjinaStevkovska <stevkovskadjina@yahoo.com>
2024-02-19 15:45:56 +0100

    Initial commit

---
+++ src/main/java/com/quickquest/quickquestapp/QuickquestApplication.java
@@ -0,0 +1,13 @@
+package com.quickquest.quickquestapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuickquestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuickquestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/quickquest/quickquestapp/QuickquestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quickquest.quickquestapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuickquestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
